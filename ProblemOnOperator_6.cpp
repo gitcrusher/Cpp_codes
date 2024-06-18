c6c740024937709d710cd46d3b538907a5399573
gitcrusher <aayushguptasoni@gmail.com>
2024-06-18 12:14:10 +0530

    problem on operators 6

---
+++ ProblemOnOperator_6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a = 10, b = 20, c = 30;
+    if (c>b>a){
+        cout<<"Y";
+    }
+    else{
+        cout<<"N";
+    }
+    return 0;
+}// here comes associativity
