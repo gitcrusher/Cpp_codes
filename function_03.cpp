e481ceecd4fb1a781f6fcb0b2995aaa28440308e
gitcrusher <aayushguptasoni@gmail.com>
2024-06-19 23:14:47 +0530

    Function 03

---
+++ function_03.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void greetMsg()
+{
+    cout<<"hi\n";
+    cout<<"Welcome to Cpp_Codes"<<" ";
+}
+void exitMsg()
+{
+    cout<<"Bye\n";
+    cout<<"Visit Again\n";
+}
+int main(){
+    greetMsg();
+    cout<<"Hope you are enjoying \n";
+    exitMsg();
+    return 0;
+}
