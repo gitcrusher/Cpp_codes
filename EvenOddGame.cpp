4acf77878c2ddb78084657757ea909e61eacadec
gitcrusher <aayushguptasoni@gmail.com>
2024-06-19 22:42:54 +0530

    Even Number Game

---
+++ EvenOddGame.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int a ;
+    cout<<"Enter your no: ";
+    cin>>a;
+    if (a%2==0){
+        cout<<"Opponent Won";
+    }
+    else
+    cout<<"You won";
+    return 0;
+}
