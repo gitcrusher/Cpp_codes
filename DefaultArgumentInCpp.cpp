aca48fc96c106f676c9c3131993287f08fe5dc9f
gitcrusher <aayushguptasoni@gmail.com>
2024-06-20 12:43:02 +0530

    default argument in functions

---
+++ DefaultArgumentInCpp.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void printDetails(int id , string name="NA", string address = "NA"){
+    cout<<"id is "<<id<<"\n";
+    cout<<"Name is "<<name<<"\n";
+    cout<<"Address is "<<address<<"\n";
+}
+int main (){
+    printDetails(101,"Sandeep","Noida");
+    cout<<"\n";
+    printDetails(201,"Shivam");
+    cout<<"\n";
+    printDetails(301);
+    return 0;
+}
