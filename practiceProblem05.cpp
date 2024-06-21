d17f161d80c6f9782099643be266a8ca31514f13
gitcrusher <aayushguptasoni@gmail.com>
2024-06-21 20:40:31 +0530

    practice problem 05

---
+++ practiceProblem05.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void fun(int x){
+    if (x==0)
+    return;
+    else{
+        cout<<"aayush\n";
+        fun(x-1);
+    }
+}
+int main (){
+    fun(3);
+    return 0;
+}
