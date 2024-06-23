e091df92f6cc18d0d9b2c54477bb507bd713c7db
gitcrusher <aayushguptasoni@gmail.com>
2024-06-23 23:51:59 +0530

    do while in cpp loops

---
+++ DoWhileLoop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int i=0;
+    do{
+        cout<<"GFG\n";
+        i++;
+    }
+    while(i<3);
+    return 0;
+}
