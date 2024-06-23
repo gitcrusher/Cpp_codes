ca2b9653f16f1d781f81651d83cf294079bb3066
gitcrusher <aayushguptasoni@gmail.com>
2024-06-23 22:58:35 +0530

    while loop 02

---
+++ whileLoop02.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter value of n: ";
+    cin>>n;
+    int i=0;
+    while(i<n){
+        cout<<"GFG\n";
+        i=i+1;
+    }
+    cout<<"Bye";
+    return 0;
+}
