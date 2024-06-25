7d9537c20a866a31cd13c634e98351e623f9a7a9
gitcrusher <aayushguptasoni@gmail.com>
2024-06-25 18:56:06 +0530

    star printiing in a single line

---
+++ patterns_StarPrintingInASingleLine.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"Enter no: ";
+    cin>>i;
+    while(i--){
+        cout<<"*"<<endl;
+    }
+    return 0;
+}
