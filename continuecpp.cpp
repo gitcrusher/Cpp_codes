c1e1d0dc70287d795ae2da17fe29d4c57aafa338
gitcrusher <aayushguptasoni@gmail.com>
2024-06-25 17:45:50 +0530

    printing all the number smaller than or equal to "x" that are nit a multiple of "n"

---
+++ continuecpp.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int x;
+    int n;
+    cout<<"Enter no: ";
+    cin>>x;
+    cout<<"No. not to be printed: ";
+    cin>>n;
+    for(int i=0;i<x+1;i++){
+        if(i%n==0){
+            continue;
+        }
+        cout<<i<<" ";
+    }
+    return 0;
+    
+}
