cbcfffb38d41e9349fb380341fcf2b96981f4400
gitcrusher <aayushguptasoni@gmail.com>
2024-06-26 23:29:03 +0530

    factorial of given number

---
+++ factorialOfANumber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int n;
+    cin>>n;
+    int ans=1;
+    while(n>0){
+        ans=ans*n;
+        n--;
+    }
+    cout<<ans;
+    return 0;
+}
