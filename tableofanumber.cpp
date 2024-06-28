10f78f0641b3bec73649b21dd8e53e687ee42a50
gitcrusher <aayushguptasoni@gmail.com>
2024-06-28 23:34:13 +0530

    tables

---
+++ tableofanumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"Enter table you want";
+    cin>>n;
+    for(int i=1;i<11;i++){
+        m=n*i;
+        cout<<m<<endl;
+    }
+    return 0;
+}
