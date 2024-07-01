5a6229f119fa72e01de1ef7d63cc93213af8c66d
gitcrusher <aayushguptasoni@gmail.com>
2024-07-01 08:22:11 +0530

    size of an array in int form

---
+++ Arrays/sizeOfAnArray.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int arr[]={10,20,30,40,50};
+    cout<<sizeof(arr);
+    return 0;
+}
+
+// so 4 bytes for every integer value and there are 5 value spots so in total there would be 20 as an ouytput.
