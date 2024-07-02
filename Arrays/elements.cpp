b9a40db237a0236fb8a2ca81e5c4110076223267
gitcrusher <aayushguptasoni@gmail.com>
2024-07-02 23:48:49 +0530

    elements

---
+++ Arrays/elements.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int arr[]={10,20,30,40,50};
+    int n = sizeof(arr)/sizeof (arr[0]);
+    cout<<n;
+    return 0;
+}
