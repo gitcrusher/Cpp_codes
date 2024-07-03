88a05ed4c938323e0cbcf319f99b6844d19680c6
gitcrusher <aayushguptasoni@gmail.com>
2024-07-03 16:50:09 +0530

    multiplication traversal

---
+++ multiplicationsTraversal.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[]={10,40,30,45};
+    for(int &x:arr)
+        x=x*2;
+    for(int x: arr){
+        cout<<x<<" ";
+    }
+    return 0;
+}
