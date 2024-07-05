e1d8e6da93d85e6f1498930b52b9bd7bce3c9119
gitcrusher <aayushguptasoni@gmail.com>
2024-07-05 22:29:14 +0530

    reference question 05

--- Arrays/refernceQuestion05.cpp
+++ Arrays/refernceQuestion05.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main (){
     string s1 = "gfg ",s2="Cources";
-    string && s3= s1+s2;
+    string &&s3= s1+s2;
     s3= "welcome To " + s3;
     cout<<s3;
     return 0;
