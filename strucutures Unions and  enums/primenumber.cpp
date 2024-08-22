85db4e0cb396679717d7fe40435af5eaddb9c575
gitcrusher <aayushguptasoni@gmail.com>
2024-08-22 09:19:32 +0530

    commit

---
+++ strucutures Unions and  enums/primenumber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cout<<"enter no.";
+    cin >> n;
+    if (n%2==0){
+        cout << "Even number entered";
+    } 
+    else 
+        cout<<"odd number added";
+    return 0;
+}
