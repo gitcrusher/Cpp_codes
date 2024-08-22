10971a122baebb451da69eba1e0767b4375f205d
gitcrusher <aayushguptasoni@gmail.com>
2024-08-22 09:31:32 +0530

    commit

--- strucutures Unions and  enums/primenumber.cpp
+++ strucutures Unions and  enums/primenumber.cpp
@@ -6,7 +6,7 @@ int main (){
     cout<<"Running:";
     cin >> n ;
     int flag = 0 ;
-    for (int i = 2 ; i <= n/2 ; i++){
+    for (int i = 2 ; i <= n-1; i++){// we can also use n/2; too 
         if(n%i==0){
             flag = 1;
             break;
