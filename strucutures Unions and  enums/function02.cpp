d453e2853159e8a718dca985a46766011f948838
gitcrusher <aayushguptasoni@gmail.com>
2024-08-23 11:20:47 +0530

    commit

--- strucutures Unions and  enums/function02.cpp
+++ strucutures Unions and  enums/function02.cpp
@@ -1,24 +1,10 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class MyClass {
-public:
-    int data;
+int main (){
+    string s;
+    getline(cin, s);
+    cout<<s;
 
-    void setData(int d) {
-        data = d;
-    }
-    friend void addToData(MyClass& obj, int value);
-};
-
-void addToData(MyClass& obj, int value) {
-    obj.data += value;
-}
-
-int main() {
-    MyClass obj1;
-    obj1.setData(20);
-    addToData(obj1, 5);
-    cout << obj1.data;
     return 0;
 }
