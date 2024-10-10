f71745ebbbf5af83a1368557bd0c5834a9a6d36d
gitcrusher <aayushguptasoni@gmail.com>
2024-10-10 10:56:03 +0530

    commit

--- Operator Overloading/AreaOfRectangle.cpp
+++ Operator Overloading/AreaOfRectangle.cpp
@@ -4,9 +4,9 @@ using namespace std;
         double length;
         double width;
         public:
-        Rectangle(double l, double w){
-            length=l;
-            width=w;
+        Rectangle(){
+            cin>>length;
+            cin>>width;
         }
         bool operator<(Rectangle &r){
             if(length*width < r.length*r.width)
@@ -17,7 +17,8 @@ using namespace std;
         
     };
 int main (){
-    Rectangle r1(4.34653,5.438768),r2(8.34653,6.438768);
+    Rectangle r1,r2;
+    
     if(r1<r2){
         cout<<"Rectangle 1 is smaller than Rectangle 2";
     }
