706374e22dd171e99c2da07f35a192dfb3f3d1ab
gitcrusher <aayushguptasoni@gmail.com>
2024-10-25 11:01:40 +0530

    commit

---
+++ virtual_distructor.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+    class A{
+        public:
+        A(){
+            cout<<"Class A Object created"<<endl;
+        }
+        ~A(){
+            cout<<"Class A Object destroyed"<<endl;
+        }
+    };
+
+    class B : public A{
+        public:
+        B(){
+            cout<<"Class B Object created"<<endl;
+        }
+        ~B(){
+            cout<<"Class B Object destroyed"<<endl;
+        }
+    };
+
+    class C : public A{
+        public:
+        C(){
+            cout<<"Class C Object created"<<endl;
+        }
+        ~C(){
+            cout<<"Class C Object destroyed"<<endl;
+        }
+    };
+
+    class D : public B, public C{
+        public:
+        D(){
+            cout<<"ClassD Object created"<<endl;
+        }
+        ~D(){
+            cout<<"ClassD Object destroyed"<<endl;
+        }
+    };
+
+int main (){
+    A*a1= new C();
+
+    delete a1;
+return 0;
+}
